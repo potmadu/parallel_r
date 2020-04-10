fd50f64d6f993b5070292e518f06578e04b17528
potmadu <potmadu@gmail.com>
2020-04-10 20:47:33 +0700

    add calcParallel function

--- parallel_r/main.R
+++ parallel_r/main.R
@@ -1,53 +1,48 @@
 library(doParallel);
 library(foreach);
 
-calc_distance_parallel = function(alerts,loc){
+input_data = read.csv('data/Airports.csv');
+packages = c();
 
-cores = detectCores(logical = FALSE)-1;
-cl = makeCluster(cores);
-registerDoParallel(cl, cores=cores);
+calcParallel = function(input_data, packages){
 
-output = alerts;
+  cores = detectCores(logical = FALSE)-1;
+  cl = makeCluster(cores);
+  registerDoParallel(cl, cores=cores);
 
-chunk.size = ceiling(nrow(alerts)/cores);
-total.chunk.size = cores * chunk.size;
-diff.chunk = total.chunk.size - nrow(alerts);
+  chunk.size = ceiling(nrow(input_data)/cores);
+  total.chunk.size = cores * chunk.size;
+  diff.chunk = total.chunk.size - nrow(input_data);
 
-added_data = 0;
+  added_data = 0;
 
-if(diff.chunk>0){
+  if(diff.chunk>0){
     for(i in 1:diff.chunk){
-        alerts = rbind(alerts,alerts[1,]);
+        input_data = rbind(input_data,input_data[1,]);
         added_data=added_data+1;
+      }
+  }
+
+  start_time = Sys.time();
+
+   res.p = foreach(i=1:cores, .combine='rbind', .packages=packages) %dopar%
+   {
+      res = matrix(0, nrow=chunk.size, ncol=1)
+      for(x in ((i-1)*chunk.size+1):(i*chunk.size)) {
+          res[x - (i-1)*chunk.size,] = distGeo(c(alerts$line_x[x],alerts$line_y[x]),loc);
+      }
+      res;
+   }
+
+    if(diff.chunk>0){
+            res.p = res.p[-((nrow(res.p)-diff.chunk)+1:nrow(res.p)),];
     }
-}
 
-start_time = Sys.time();
+    end_time = Sys.time();
+    print('elapsed time:');
+    print(end_time - start_time);
 
- res2.p = foreach(i=1:cores, .combine='rbind', .packages='geosphere') %dopar%
- {
-    res = matrix(0, nrow=chunk.size, ncol=1)
-    for(x in ((i-1)*chunk.size+1):(i*chunk.size)) {
-        res[x - (i-1)*chunk.size,] = distGeo(c(alerts$line_x[x],alerts$line_y[x]),loc);
-    }
-    res;
- }
-
-end_time = Sys.time();
-end_time - start_time;
-
-if(diff.chunk>0){
-        res2.p = res2.p[-((nrow(res2.p)-diff.chunk)+1:nrow(res2.p)),];
-}
-
-output$jarak = res2.p;
-
-stopImplicitCluster();
-stopCluster(cl);
-
-return(output);
+    stopImplicitCluster();
+    stopCluster(cl);
 
 }
-
-alerts_before_dist = calc_distance_parallel(alerts_before,pasar_tanahabang);
-alerts_after_dist = calc_distance_parallel(alerts_after,pasar_tanahabang);
