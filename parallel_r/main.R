2bbcf3ecf8a2bd7c0ca5528ce9856b4c88bb9670
Potmadu <potmadu@gmail.com>
2020-04-11 22:24:37 +0700

    Add calcParallel functions

--- parallel_r/main.R
+++ parallel_r/main.R
@@ -1,12 +1,15 @@
-library(doParallel);
-library(foreach);
+library(AppliedPredictiveModeling);
+library(dplyr);
 
-test;
+data(abalone);
+packages = c('dplyr','AppliedPredictiveModeling');
 
-input_data = read.csv('data/Airports.csv');
-packages = c();
+input_data = abalone;
 
 calcParallel = function(input_data, packages){
+
+  library(doParallel);
+  library(foreach);
 
   cores = detectCores(logical = FALSE)-1;
   cl = makeCluster(cores);
@@ -25,13 +28,12 @@ calcParallel = function(input_data, packages){
       }
   }
 
-  start_time = Sys.time();
-
    res.p = foreach(i=1:cores, .combine='rbind', .packages=packages) %dopar%
    {
       res = matrix(0, nrow=chunk.size, ncol=1)
       for(x in ((i-1)*chunk.size+1):(i*chunk.size)) {
-          res[x - (i-1)*chunk.size,] = distGeo(c(alerts$line_x[x],alerts$line_y[x]),loc);
+          #res[x - (i-1)*chunk.size,] = distGeo(c(alerts$line_x[x],alerts$line_y[x]),loc);
+          res[x - (i-1)*chunk.size,] = colnames(input_data[which.max(input_data[x,])])
       }
       res;
    }
@@ -40,11 +42,16 @@ calcParallel = function(input_data, packages){
             res.p = res.p[-((nrow(res.p)-diff.chunk)+1:nrow(res.p)),];
     }
 
-    end_time = Sys.time();
-    print('elapsed time:');
-    print(end_time - start_time);
-
     stopImplicitCluster();
     stopCluster(cl);
 
+    return(res.p);
+
 }
+
+system.time(calcParallel(abalone,packages))  
+
+for (i in 1:nrow(abalone)){
+  abalone$res = colnames(abalone[which.max(abalone[i,])])
+}
+
