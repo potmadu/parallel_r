9fb92ddffa12a84426b9702ae2fd0d665ef13694
potmadu <potmadu@gmail.com>
2020-05-08 01:11:54 +0700

    add hyperparameters optimization (sklearn.gridcv, sklearn.randomcv,hyperopt.tpe)

--- mllib_python/regression_otp.py
+++ mllib_python/regression_otp.py
@@ -55,77 +55,218 @@ target = dat['X5'].copy();
 data = dat.drop('X5',1);
 dtrain = xgb.DMatrix(data,target);
 
-for iter in range(20:30):
-	i = iter-1;
-	param = tuning_grid.iloc[[i]].reset_index();
-	params = {
-			'objective': 'reg:squarederror',
-			'eval_metric': 'rmse',
-			'tree_method': 'exact'
-		}
-	params2 = {
-		'objective': 'reg:squarederror',
-		'eval_metric': 'rmse',
-		'tree_method': 'exact',
-		'subsample' : 0.75,
-		'max_depth' : 10,
-		'reg_lambda' : 10,
-		'learning_rate' : 1
-	}
-	params.update(param.transpose().to_dict().get(0));
-	params_df = pd.DataFrame(list(params.items()),columns = ['Params','Value']);
-	cv = xgb.cv(
-		params2,
-		dtrain,
-		metrics='rmse',
-		num_boost_round=100,
-		nfold=10,
-		seed=12345,
-		early_stopping_rounds=10,
-		shuffle=True,
-		callbacks=[xgb.callback.print_evaluation(show_stdv=True)]
-	)
+#################################
 
-####################
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+import multiprocessing
 
-params = {
-	'objective': 'reg:squarederror',
-	'eval_metric': 'rmse',
-	'tree_method': 'exact',
-	'subsample': 0.75,
-	'max_depth': 10,
-	'reg_lambda': 10,
-	'learning_rate': 1
+n_jobs = multiprocessing.cpu_count()-1;
+
+parameters = {'nthread':[4],
+              'objective':['binary:logistic'],
+              'learning_rate': [0.05], #so called `eta` value
+              'max_depth': [6],
+              'min_child_weight': [11],
+              'silent': [1],
+              'subsample': [0.8],
+              'colsample_bytree': [0.7],
+              'n_estimators': [5], #number of trees, change it to 1000 for better results
+              'missing':[-999],
+              'seed': [1337]}
+
+params_sample = {
+	'tree_method': ['exact'],
+	'subsample': [0.25, 0.5],
+	'learning_rate': [0.01,0.1]
+};
+
+params_complete = {
+	'tree_method': ['exact'],
+	'subsample': [0.25, 0.5, 0.75],
+	'max_depth': [3,5,10],
+	'reg_lambda': [0.1,1,10],
+	'learning_rate': [0.01,0.1,1]
+};
+
+xgb_model = xgb.XGBRegressor(
+	objective = 'reg:squarederror',
+	n_estimators = 100,
+	early_stopping_rounds=10
+);
+
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=12345);
+
+clf = GridSearchCV(xgb.XGBRegressor(), params_sample, n_jobs=n_jobs,
+                   cv=KFold(10,True,12345),
+                   scoring='r2',
+                   verbose=2, refit=True);
+
+clf.fit(X_train, y_train);
+
+pred = clf.predict(X_test);
+sklearn.metrics.mean_squared_error(y_test,pred);
+
+sklearn.metrics.r2_score(y_test,pred)
+Out[22]: 0.5029078009373649
+
+#############################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+r_squared = sklearn.metrics.r2_score(y_test,pred);
+plt.scatter(y_test,pred);
+plt.xlabel('Actual values');
+plt.ylabel('Predicted values');
+
+plt.plot(np.unique(y_test), np.poly1d(np.polyfit(y_test, pred, 1))(np.unique(y_test)));
+
+plt.text(0.6, 0.5, 'R-squared = %0.2f' % r_squared);
+plt.show();
+
+#############################################
+
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+import multiprocessing
+from scipy import stats
+import sklearn
+
+param_dist = {'n_estimators': stats.randint(150, 1000),
+              'learning_rate': stats.uniform(0.01, 0.6),
+              'subsample': stats.uniform(0.3, 0.9),
+              'max_depth': [3, 4, 5, 6, 7, 8, 9],
+              'colsample_bytree': stats.uniform(0.5, 0.9),
+              'min_child_weight': [1, 2, 3, 4]
+             }
+
+params_dist = {
+	'tree_method': ['exact'],
+	'subsample': stats.uniform(0.3, 0.9),
+	'learning_rate': stats.uniform(0.01, 0.6)
+};
+
+xgb_model = xgb.XGBRegressor(
+	objective = 'reg:squarederror',
+	n_estimators = 100,
+	early_stopping_rounds=10
+);
+
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=12345);
+
+n_jobs = multiprocessing.cpu_count()-1;
+
+clf = RandomizedSearchCV(xgb_model,
+						 params_dist,
+						n_iter=10,
+						n_jobs=n_jobs,
+						 cv=KFold(10,True,12345),
+						 random_state=12345,
+						scoring='r2',
+                   		verbose=2, refit=True);
+
+clf.fit(X_train, y_train);
+pred = clf.predict(X_test);
+sklearn.metrics.mean_squared_error(y_test,pred);
+sklearn.metrics.r2_score(y_test,pred)
+0.506275680859126
+
+####################################################
+### NOT WORKING YET
+####################################################
+
+from hyperopt import hp
+from hyperopt.pyll import scope
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+# XGB parameters
+xgb_reg_params = {
+    'learning_rate':    hp.choice('learning_rate',    np.arange(0.05, 0.31, 0.05)),
+    'max_depth':        hp.choice('max_depth',        np.arange(5, 16, 1, dtype=int)),
+    'min_child_weight': hp.choice('min_child_weight', np.arange(1, 8, 1, dtype=int)),
+    'colsample_bytree': hp.choice('colsample_bytree', np.arange(0.3, 0.8, 0.1)),
+    'subsample':        hp.uniform('subsample', 0.8, 1),
+    'n_estimators':     100,
+    'objective' : 'reg:squarederror'
+}
+xgb_reg_params2 = {
+    'learning_rate':    hp.loguniform('learning_rate',np.log(0.0001), np.log(0.5)) - 0.0001,
+    'max_depth':        scope.int(hp.uniform('max_depth',1, 11)),
+    'min_child_weight': scope.int(hp.loguniform('min_child_weight',np.log(1), np.log(100))),
+    'colsample_bytree': hp.uniform('colsample_bytree',0.5, 1),
+    'subsample':        hp.uniform('subsample', 0.8, 1),
+    'n_estimators':     100,
+    'objective' : 'reg:squarederror'
 }
 
-gridsearch_params = [
-    (subsample, max_depth, reg_lambda, learning_rate)
-    for subsample in map(float, [0.25, 0.5, 0.75])
-    for max_depth in map(int, [3,5,10])
-	for reg_lambda in map(float, [0.1,1,10])
-	for learning_rate in map(float, [0.01,0.1,1])
-]
+xgb_fit_params = {
+    'eval_metric': 'rmse',
+    'early_stopping_rounds': 10,
+    'verbose': False
+}
+xgb_para = dict()
+xgb_para['reg_params'] = xgb_reg_params2
+xgb_para['fit_params'] = xgb_fit_params
+xgb_para['loss_func' ] = lambda y, pred: np.sqrt(mean_squared_error(y, pred))
 
-min_mae = float("Inf")
-best_params = None
-for max_depth, min_child_weight in gridsearch_params:
-    print("CV with max_depth={}, min_child_weight={}".format(
-                             max_depth,
-                             min_child_weight))    # Update our parameters
-    params['max_depth'] = max_depth
-    params['min_child_weight'] = min_child_weight    # Run CV
-    cv_results = xgb.cv(
-        params,
-        dtrain,
-        num_boost_round=num_boost_round,
-        seed=42,
-        nfold=5,
-        metrics={'mae'},
-        early_stopping_rounds=10
-    )
-    mean_mae = cv_results['test-mae-mean'].min()
-    boost_rounds = cv_results['test-mae-mean'].argmin()
-    print("\tMAE {} for {} rounds".format(mean_mae, boost_rounds))
-    if mean_mae < min_mae:
-        min_mae = mean_mae
-        best_params = (max_depth,min_child_weight)print("Best params: {}, {}, MAE: {}".format(best_params[0], best
+import xgboost as xgb
+from hyperopt import fmin, tpe, STATUS_OK, STATUS_FAIL, Trials
+
+class HPOpt(object):
+
+    def __init__(self, x_train, x_test, y_train, y_test):
+        self.x_train = x_train
+        self.x_test  = x_test
+        self.y_train = y_train
+        self.y_test  = y_test
+
+    def process(self, fn_name, space, trials, algo, max_evals):
+        fn = getattr(self, fn_name)
+        try:
+            result = fmin(fn=fn, space=space, algo=algo, max_evals=max_evals, trials=trials)
+        except Exception as e:
+            return {'status': STATUS_FAIL,
+                    'exception': str(e)}
+        return result, trials
+
+    def xgb_reg(self, para):
+        reg = xgb.XGBRegressor(**para['reg_params'])
+        return self.train_reg(reg, para)
+
+    def train_reg(self, reg, para):
+        reg.fit(self.x_train, self.y_train,
+                eval_set=[(self.x_train, self.y_train), (self.x_test, self.y_test)],
+                **para['fit_params'])
+        pred = reg.predict(self.x_test)
+        loss = para['loss_func'](self.y_test, pred)
+        return {'loss': loss, 'status': STATUS_OK}
+
+obj = HPOpt(X_train, X_test, y_train, y_test)
+
+xgb_opt = obj.process(fn_name='xgb_reg', space=xgb_para, trials=Trials(), algo=tpe.suggest, max_evals=10)
+
+model = xgb.XGBRegressor(
+    colsample_bytree = 0.9760113887185847,
+    learning_rate = 0.28820148093631176,
+    max_depth = 7,
+    min_child_weight = 7.619803211294129,
+    subsample = 0.9633969560904857,
+    n_estimators = 100,
+    objective = 'reg:squarederror',
+    eval_metric = 'rmse',
+    early_stopping_rounds = 10,
+    verbose = False
+);
+
+clf = model.fit(X_train,y_train);
+
+preds = clf.predict(X_test);
+
+print(r2_score(y_test,preds));
+0.5084898349358241
+
